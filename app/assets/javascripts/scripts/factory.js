4a2d8c4870354967afef5cf85228c845f4d9ca61
root <root@carfocusautomd.com>
2015-07-10 04:49:30 -0400

    server push unicorn

--- app/assets/javascripts/scripts/factory.js
+++ app/assets/javascripts/scripts/factory.js
@@ -2,8 +2,8 @@ app
   .factory( "CarServer" , [
     "$http",
     function factory ( $http ) {
-      // var host = "http://128.199.117.211/";
-      var host = "http://localhost:3000";
+    var host = "http://128.199.117.211/";
+      //var host = "http://128.199.117.211:300";
 
       this.request  = function request ( method , path , callback , data ) {
         $http[ method ]( host + path , ( data || { } ) )
